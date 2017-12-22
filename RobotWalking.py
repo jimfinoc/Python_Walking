67ad2e8fb5a466ed5b5af5bac4053309574be711
J <jimfinoc@users.noreply.github.com>
2017-12-22 11:48:52 -0500

    distance

--- RobotWalking.py
+++ RobotWalking.py
@@ -169,7 +169,7 @@ while True:
     if counter%5:
         print ('Step LF RF LB RB')
     counter += 1
-    print ('{0:4d} {0:4d} {0:4d} {0:4d}'.format(x+base[RF][0]+footRF[0],y-base[RF][1]-footRF[1]))
+    print ('{0:4d} {0:4d}'.format(x+base[RF][0]+footRF[0],y-base[RF][1]-footRF[1]))
 
     canvas.create_polygon(x+box[LB][0],y-box[LB][1], x+box[LF][0],y-box[LF][1],outline="red")
     canvas.create_polygon(x+box[LF][0],y-box[LF][1], x+box[RF][0],y-box[RF][1],outline="red")
