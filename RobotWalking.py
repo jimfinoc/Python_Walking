5f66c91741bbeca26ed3e899705ae8caeaad7768
J <jimfinoc@users.noreply.github.com>
2017-12-22 11:38:39 -0500

    3

--- RobotWalking.py
+++ RobotWalking.py
@@ -161,10 +161,10 @@ while True:
         # canvas.create_polygon(x+base[RF][0]+footRF[stepLF][0],y-base[RF][1]-footRF[stepRF][1],x+base[RF][0]+footRF[stepRF][0],y-base[RF][1]-footRF[stepRF][1],outline="yellow",fill="yellow")
 
     ### draw the body and center of mass of the robot
-    canvas.create_polygon(x+base[LF][0],y-base[LF][1], x+base[LF][0]+footLF[stepLF][0],y-base[LF][1]-footLF[stepLF][1],outline="black")
-    canvas.create_polygon(x+base[RF][0],y-base[RF][1], x+base[RF][0]+footRF[stepRF][0],y-base[RF][1]-footRF[stepRF][1],outline="black")
-    canvas.create_polygon(x+base[LB][0],y-base[LB][1], x+base[LB][0]+footLB[stepLB][0],y-base[LB][1]-footLB[stepLB][1],outline="black")
-    canvas.create_polygon(x+base[RB][0],y-base[RB][1], x+base[RB][0]+footRB[stepRB][0],y-base[RB][1]-footRB[stepRB][1],outline="black")
+    # canvas.create_polygon(x+base[LF][0],y-base[LF][1], x+base[LF][0]+footLF[stepLF][0],y-base[LF][1]-footLF[stepLF][1],outline="black")
+    # canvas.create_polygon(x+base[RF][0],y-base[RF][1], x+base[RF][0]+footRF[stepRF][0],y-base[RF][1]-footRF[stepRF][1],outline="black")
+    # canvas.create_polygon(x+base[LB][0],y-base[LB][1], x+base[LB][0]+footLB[stepLB][0],y-base[LB][1]-footLB[stepLB][1],outline="black")
+    # canvas.create_polygon(x+base[RB][0],y-base[RB][1], x+base[RB][0]+footRB[stepRB][0],y-base[RB][1]-footRB[stepRB][1],outline="black")
 
 
     canvas.create_polygon(x+box[LB][0],y-box[LB][1], x+box[LF][0],y-box[LF][1],outline="red")
