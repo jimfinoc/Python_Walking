f99600bc7b3ca7ba504c5c3b811f9150397f036b
J <jimfinoc@users.noreply.github.com>
2017-12-22 11:58:33 -0500

    1

--- RobotWalking.py
+++ RobotWalking.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import os
 
 # import pygame
 # import pygame.mixer
@@ -172,7 +173,8 @@ while True:
     canvas.create_polygon(x+base[RF][0],y-base[RF][1], x+base[RF][0]+footRF[0],y-base[RF][1]-footRF[1],outline="black")
     # canvas.create_polygon(x+base[LB][0],y-base[LB][1], x+base[LB][0]+footLB[stepLB][0],y-base[LB][1]-footLB[stepLB][1],outline="black")
     # canvas.create_polygon(x+base[RB][0],y-base[RB][1], x+base[RB][0]+footRB[stepRB][0],y-base[RB][1]-footRB[stepRB][1],outline="black")
-    if counter%5==0:
+    if counter%23==0:
+        os.system("clear")
         print ('Step LF RF LB RB')
     counter += 1
     print ('{0:4d} {0:4d}'.format(x+base[RF][0]+footRF[0],y-base[RF][1]-footRF[1]))
