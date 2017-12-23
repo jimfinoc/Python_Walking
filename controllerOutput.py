59354256f7724fd44c76a9ab4b518d78835b9af1
J <jimfinoc@users.noreply.github.com>
2017-12-23 12:49:07 -0500

    yes

--- controllerOutput.py
+++ controllerOutput.py
@@ -15,6 +15,29 @@ done = False
 
 clock = pygame.time.Clock()
 
+
+class TextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def print(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, [self.x, self.y])
+        self.y += self.line_height
+
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
+    def indent(self):
+        self.x += 10
+
+    def unindent(self):
+        self.x -= 10
+
+
 # Get ready to print
 textPrint = TextPrint()
 
