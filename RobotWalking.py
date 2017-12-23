a682fe9e17efae70437d17eebf9e5a83a68350aa
J <jimfinoc@users.noreply.github.com>
2017-12-22 19:04:03 -0500

    2

--- RobotWalking.py
+++ RobotWalking.py
@@ -73,8 +73,11 @@ RF = 2
 RB = 3
 xVar = 0
 yVar = 1
+zVar = 2
 lineCounter = 0
 step = {}
+center = 0
+
 # step[LB] = [ (stepX+2)%8 ,(stepY+2)%8]
 # step[LF] = [ (stepX+0)%8 ,(stepY+0)%8]
 # step[RF] = [ (stepX+4)%8 ,(stepY+4)%8]
@@ -94,20 +97,24 @@ box = ([-50,-50],[-50,50],[50,50],[50,-50])
 base = ([-45,-45],[-45,45],[45,45],[45,-45])
 
 # footRF = ([ 20, 0,1],[ 20, 15,0],[ 20, 10,0],[ 20, 5,0],[ 20, 0,0],[ 20, -5,0],[ 20, -10,0],[ 20, -15,0],[],[])
-footTable = (
-    # first,0 is the step offset, then,1 is the first limit, then,7 is the other bound
-    [#run motion step of 5
-        # [ 0,5,0],[ 0, 15,0],[],[],[],[],[],[ 20, -15,0],[],[],
-    ],
-    [#walk motion
-        [0,1,0],[ 0, 3,0],[],[],[],[],[],[ 20, -3,0],[],[],
-    ],
-    [#lateral motion
-        [ 1,0,0],[ 3,  0,0],[],[],[],[],[],[ -3,   0,0],[],[]
-    ],[#turning motion
-        [0,0,1],[0,0,0]
-    ]
-    )
+footTable = {}
+footTable[center] = {}
+footTable[center][RFgot  = {}
+[0,0,1]
+# (
+#     # first,0 is the step offset, then,1 is the first limit, then,7 is the other bound
+#     [#run motion step of 5
+#         # [ 0,5,0],[ 0, 15,0],[],[],[],[],[],[ 20, -15,0],[],[],
+#     ],
+#     [#walk motion
+#         [0,0,1],[ 0, 3,0],[],[],[],[],[],[ 20, -3,0],[],[],
+#     ],
+#     [#lateral motion
+#         [ 0,0,1],[ 3,  0,0],[],[],[],[],[],[ -3,   0,0],[],[]
+#     ],[#turning motion
+#         [0,0,1],[0,0,0]
+#     ]
+#     )
 #initial positions of the feed
 # foot = ([-20,0,0],[-20,0,0],[20,0,0],[20,0,0])
 foot = {}
@@ -181,17 +188,19 @@ while True:
     step[RF] = [ (stepX+4)%8 ,0]
     step[RB] = [ (stepX+6)%8 ,0]
 
-    legUp = False
-    if step[LB]
+    legsDown = False
+    if step[RF][xVar] == 0 and legsDown:
+        foot[RF][xVar] = footTable
+
 
     ### draw the four foot position on the canvas
-    if foot[LF][2]==0 and foot[RF][2]==0 and foot[LB][2]==0 and foot[RB][2]==0:
+    if foot[LF][zVar]==0 and foot[RF][zVar]==0 and foot[LB][zVar]==0 and foot[RB][zVar]==0:
         # footRF = [footRF[0]+
         canvas.create_polygon(
-            x+base[LF][0]+foot[LF][0],y-base[LF][1]-foot[LF][1],
-            x+base[RF][0]+foot[RF][0],y-base[RF][1]-foot[RF][1],
-            x+base[RB][0]+foot[RB][0],y-base[RB][1]-foot[RB][1],
-            x+base[LB][0]+foot[LB][0],y-base[LB][1]-foot[LB][1],
+            x+base[LF][xVar]+foot[LF][xVar],y-base[LF][yVar]-foot[LF][yVar],
+            x+base[RF][xVar]+foot[RF][xVar],y-base[RF][yVar]-foot[RF][yVar],
+            x+base[RB][xVar]+foot[RB][xVar],y-base[RB][yVar]-foot[RB][yVar],
+            x+base[LB][xVar]+foot[LB][xVar],y-base[LB][yVar]-foot[LB][yVar],
             outline="yellow",fill="yellow")
     else: # this just draws one of four triangles for the 3 down feet
         if footLF[stepLF][2]==0 and footRF[stepRF][2]==0 and footLB[stepLB][2]==0:
