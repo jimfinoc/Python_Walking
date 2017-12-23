5250f5adde8c794ea731206502617e19aca244bd
J <jimfinoc@users.noreply.github.com>
2017-12-23 12:42:30 -0500

    Ee

--- controllerOutput.py
+++ controllerOutput.py
@@ -2,3 +2,8 @@ import pygame
 pygame.init()
 print "pygame.joystick.get_count()"
 print pygame.joystick.get_count()
+
+size = [50, 50]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("End Effector Placement")
