c7b37974a806bf28670ee3ddc47ca68515692cd5
J <jimfinoc@users.noreply.github.com>
2017-12-22 21:49:53 -0500

    W

--- RobotWalking.py
+++ RobotWalking.py
@@ -217,25 +217,25 @@ while True:
             x+base[LB][xVar]+foot[LB][xVar],y-base[LB][yVar]-foot[LB][yVar],
             outline="yellow",fill="yellow")
     else: # this just draws one of four triangles for the 3 down feet
-        if footLF[stepLF][2]==0 and footRF[stepRF][2]==0 and footLB[stepLB][2]==0:
+        if foot[LF][zVar]==0 and foot[RF][zVar]==0 and foot[LB][zVar]==0:
             canvas.create_polygon(
                 x+base[LF][0]+foot[LF][0],y-base[LF][1]-foot[LF][1],
                 x+base[RF][0]+foot[RF][0],y-base[RF][1]-foot[RF][1],
                 x+base[LB][0]+foot[LB][0],y-base[LB][1]-foot[LB][1],
                 outline="yellow",fill="yellow")
-        if footLF[stepLF][2]==0 and footRF[stepRF][2]==0  and footRB[stepRB][2]==0:
+        if foot[LF][zVar]==0 and foot[RF][zVar]==0 and foot[RB][zVar]==0:
+        canvas.create_polygon(
+                x+base[LF][0]+foot[LF][0],y-base[LF][1]-foot[LF][1],
+                x+base[RB][0]+foot[RB][0],y-base[RB][1]-foot[RB][1],
+                x+base[LB][0]+foot[LB][0],y-base[LB][1]-foot[LB][1],
+                outline="yellow",fill="yellow")
+        if foot[LF][zVar]==0 and foot[LB][zVar]==0 and foot[RB][zVar]==0:
             canvas.create_polygon(
                 x+base[LF][0]+foot[LF][0],y-base[LF][1]-foot[LF][1],
                 x+base[RB][0]+foot[RB][0],y-base[RB][1]-foot[RB][1],
                 x+base[LB][0]+foot[LB][0],y-base[LB][1]-foot[LB][1],
                 outline="yellow",fill="yellow")
-        if footLF[stepLF][2]==0 and footLB[stepLB][2]==0 and footRB[stepRB][2]==0:
-            canvas.create_polygon(
-                x+base[LF][0]+foot[LF][0],y-base[LF][1]-foot[LF][1],
-                x+base[RB][0]+foot[RB][0],y-base[RB][1]-foot[RB][1],
-                x+base[LB][0]+foot[LB][0],y-base[LB][1]-foot[LB][1],
-                outline="yellow",fill="yellow")
-        if footRF[stepRF][2]==0 and footLB[stepLB][2]==0 and footRB[stepRB][2]==0:
+        if foot[RF][zVar]==0 and foot[LB][zVar]==0 and foot[RB][zVar]==0:
             canvas.create_polygon(
                 x+base[RF][0]+foot[RF][0],y-base[RF][1]-foot[RF][1],
                 x+base[RB][0]+foot[RB][0],y-base[RB][1]-foot[RB][1],
